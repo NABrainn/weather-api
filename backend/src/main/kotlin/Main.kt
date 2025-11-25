2976c48d71073ba426879dab037f2090156f8aab
Dolan <sdolazinski@gmail.com>
2025-11-25 17:56:17 +0100

    cors

--- backend/src/main/kotlin/Main.kt
+++ backend/src/main/kotlin/Main.kt
@@ -1,10 +1,17 @@
 import controller.WeatherController
 import io.javalin.Javalin
+import io.javalin.plugin.bundled.CorsPluginConfig
 
 fun main() {
     DatabaseFactory.init()
 
     val port = System.getenv("SERVER_PORT")?.toIntOrNull() ?: 8080
-    val app = Javalin.create().start(port)
+    val app = Javalin.create { config ->
+        config.plugins.enableCors { cors ->
+            cors.add { rule ->
+                rule.allowHost("http://localhost:5173")
+            }
+        }
+    }.start(port)
     WeatherController.register(app)
 }
